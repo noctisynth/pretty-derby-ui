8394a13cc52da54a373d0ca4e28283684ce0ad3f
苏向夜 <fu050409@163.com>
2024-05-21 13:59:10 +0800

    fix(toast): add toast instance at `App.vue`

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import Toast from 'primevue/toast'
 </script>
 
 <template>
   <main>
+    <Toast class="w-10rem max-w-90%"></Toast>
     <RouterView />
   </main>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
