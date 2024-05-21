7c09aa2bd2c3ce4c240ad97385ed527fb9aa38cf
苏向夜 <fu050409@163.com>
2024-05-21 13:58:33 +0800

    feat(persist): add login check before upload page

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,20 +1,27 @@
 <script setup lang="ts">
-import Toast from 'primevue/toast';
 import { onMounted } from 'vue';
 import { useAccountStore } from '../stores/account';
 import router from '../routers';
+import { useToast } from 'primevue/usetoast';
 
+const toast = useToast();
 const accountStore = useAccountStore();
 
-onMounted(() => {
-    if (accountStore.isLoggedIn())
-        router.push('/dashboard');
+onMounted(async () => {
+    if (accountStore.isLoggedIn()) {
+        const res = await accountStore.acquire()
+        if (res.status)
+            router.push('/dashboard');
+        else {
+            accountStore.account = null;
+            toast.add({ severity: 'error', summary: 'Error', detail: "Failed to acquire session: " + res.msg });
+        }
+    }
 });
 </script>
 
 <template>
     <div class="w-full h-full">
-        <Toast class="w-10rem max-w-90%"></Toast>
         <Login></Login>
     </div>
 </template>
