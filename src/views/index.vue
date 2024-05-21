00f953fa60b57c11091e8b7f0456a0fe7110449f
苏向夜 <fu050409@163.com>
2024-05-21 19:16:45 +0800

    feat(home): add home page

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import { useAccountStore } from '../stores/account';
-import router from '../routers';
+import { useRouter } from 'vue-router';
 import { useToast } from 'primevue/usetoast';
 
 const toast = useToast();
+const router = useRouter();
 const accountStore = useAccountStore();
+const msg = ref<string>('Please wait while we acquire your session...');
 
 onMounted(async () => {
     if (accountStore.isLoggedIn()) {
@@ -17,12 +19,17 @@ onMounted(async () => {
             toast.add({ severity: 'error', summary: 'Error', detail: "Failed to acquire session: " + res.msg });
         }
     }
+    else
+        router.push('/login');
 });
 </script>
 
 <template>
     <div class="w-full h-full">
-        <Login></Login>
+        <div class="flex flex-col gap-3rem justify-center items-center h-screen">
+            <ProgressSpinner />
+            <span class="font-bold">{{ msg }}</span>
+        </div>
     </div>
 </template>
 
---
+++ src/views/login.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="w-full h-full">
+        <LoginPanel></LoginPanel>
+    </div>
+</template>
+
+<style scoped></style>
